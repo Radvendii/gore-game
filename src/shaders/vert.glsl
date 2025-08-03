ab72abd08e5432f3ee864434b67ee687b0421dce
Taeer Bar-Yam <taeer@bar-yam.me>
2025-08-03 13:14:55 -0400

    reformat

--- src/shaders/vert.glsl
+++ src/shaders/vert.glsl
@@ -1,9 +1,9 @@
 #version 330 core
 #extension GL_ARB_explicit_uniform_location : require
-layout (location=0) in vec2 aPos;
-layout (location=1) in vec3 aColor;
+layout(location = 0) in vec2 aPos;
+layout(location = 1) in vec3 aColor;
 
-layout (location=2) uniform mat3x3 transform;
+layout(location = 2) uniform mat3x3 transform;
 
 out vec3 color;
 
